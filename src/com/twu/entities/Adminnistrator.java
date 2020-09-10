136622a64c8ea46330fce6b1b6245f5bc361abfa
lpdft <772346730@qq.com>
2020-09-10 16:32:41 +0800

    [ruyue] feat:finish Administrator class

--- src/com/twu/entities/Adminnistrator.java
+++ src/com/twu/entities/Adminnistrator.java
@@ -1,4 +1,14 @@
 package com.twu.entities;
 
 public class Adminnistrator {
+    public Adminnistrator() {
+    }
+
+    public String getName() {
+        return "admin";
+    }
+
+    public String getPassWord() {
+        return "123456";
+    }
 }
