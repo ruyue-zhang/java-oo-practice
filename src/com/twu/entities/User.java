3b4f53f3caee34b7db2abc7f114883847ccb3cec
lpdft <772346730@qq.com>
2020-09-10 16:29:55 +0800

    [ruyue] feat:finish User class

--- src/com/twu/entities/User.java
+++ src/com/twu/entities/User.java
@@ -1,4 +1,27 @@
 package com.twu.entities;
 
 public class User {
+    private String userName;
+    private int voteNumber;
+
+    public User(String userName) {
+        this.userName = userName;
+        this.voteNumber = 10;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getVoteNumber() {
+        return voteNumber;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setVoteNumber(int voteNumber) {
+        this.voteNumber = voteNumber;
+    }
 }
